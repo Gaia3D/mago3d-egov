425960199db44330850d024f736efd7a04aacc01
hjchoi <hjchoi@gaia3d.com>
2018-05-15 17:55:31 +0900

    테스트 케이스 수정-생성자

--- src/test/java/vo/Page.java
+++ src/test/java/vo/Page.java
@@ -7,119 +7,145 @@ import java.util.List;
  * @author HJCHOI
  * 페이징 처리
  */
-/**
- * @author HJCHOI
- *
- */
-/**
- * @author HJCHOI
- *
- */
 public class Page {
 	
 	/** 목록의 총 건수  **/
-	long totalCount; 
+	private long totalCount; 
 	
-	/** 화면에 보여질 페이지 수  **/
-	long pageNo;
+	/** 화면에 보여질 페이지 갯수  **/
+	private long pageCount = 10l;
+	
+	/** 화면에 보여질 리스트 갯수  **/
+	private long listCount = 10l;
 	
 	/** 전체 페이지 갯수  **/
-	long totalPage;
+	private long totalPage;
 	
 	/** 현재 페이지 번호  **/
-	long currentPage;
+	private long currentPage;
 	
 	/** 현재 페이지의 시작 번호  **/
-	long startPage;
+	private long startPage;
 	
 	/** 현재 페이지의 끝 번호  **/
-	long endPage;
-	
-	/** 현재 페이지 목록 리스트  **/
-	List<Long> dataList;
+	private long endPage;
 	
 	/** 다음 페이지  **/
-	long nextPage;
+	private long nextPage;
 	
 	/** 이전 페이지  **/
-	long prePage;
+	private long prePage;
 	
-	public Page(long totalCount, long pageNo) {
-		currentPage = 1;
-		this.pageNo = 10;
-		this.totalPage = ((totalCount -1) / pageNo) + 1;
+	public Page() {
+		
 	}
 	
-	public Page(long totalCount, long pageNo, long currentPage) {
-		this.pageNo = 10;
-		this.currentPage = currentPage;
-		this.totalPage = ((totalCount -1) / pageNo) + 1;
-		this.startPage = currentPage * pageNo + 1 ;
-		this.nextPage = currentPage + 1;
-		this.prePage = currentPage - 1;
+	public Page(long totalCount, long pageCount, long listCount) {
+		this.totalCount = totalCount;
+		this.listCount = listCount;
+		this.pageCount = pageCount;
+		init();
+	}
+	
+	private void init() {
+		this.totalPage = (totalCount -1) / listCount + 1 ;
 	}
 
-	public void setCurrentPage(long currentPage) {
-		this.currentPage = currentPage;
-	}
-	
 	public long getTotalCount() {
 		return totalCount;
+	}
+
+	public void setTotalCount(long totalCount) {
+		this.totalCount = totalCount;
+	}
+
+	public long getPageCount() {
+		return pageCount;
+	}
+
+	public void setPageCount(long pageCount) {
+		this.pageCount = pageCount;
+	}
+
+	public long getListCount() {
+		return listCount;
+	}
+
+	public void setListCount(long listCount) {
+		this.listCount = listCount;
+	}
+
+	public long getTotalPage() {
+		return totalPage;
+	}
+
+	public void setTotalPage(long totalPage) {
+		this.totalPage = totalPage;
 	}
 
 	public long getCurrentPage() {
 		return currentPage;
 	}
-	
+
+	public void setCurrentPage(long currentPage) {
+		this.currentPage = currentPage;
+		this.startPage = currentPage * listCount + 1;
+		this.endPage = startPage + pageCount -1;
+		this.nextPage = currentPage + 1;
+		this.prePage = currentPage - 1;
+	}
+
 	public long getStartPage() {
 		return startPage;
 	}
-	
-	public long getNextPage(){
-		return nextPage;
+
+	public void setStartPage(long startPage) {
+		this.startPage = startPage;
 	}
-	
-	public long getPrePage(){
-		return prePage;
+
+	public long getEndPage() {
+		return endPage;
+	}
+
+	public void setEndPage(long endPage) {
+		this.endPage = endPage;
 	}
 
 	/**
-	 * 현재 페이지의 시작 번호를 가져오는 메소드
-	 * @param currentPage
-	 * @return
-	 */
-	public long getStartPage(long currentPage) {
-		long startPage = currentPage * pageNo + 1 ;
-		this.startPage = startPage;
-		return startPage;
-	}
-	
-	/**
-	 * 현재 페이지의 마지막 번호를 가져오는 메소드
-	 * @param currentPage
-	 * @return
-	 */
-	public long getEndPage(long currentPage) {
-		long endPage = (getStartPage(currentPage) + pageNo) - 1;
-		
-		return endPage;
-	}
-	
-	/**
 	 * 현재 페이지 목록을 가져오는 메소드
+	 * @param pageIndex
 	 * @return
 	 */
-	public List<Long> getdataList(long pageIndex) {
+	public List<Long> getDataList(long pageIndex) {
 		List<Long> list = new ArrayList<>();
+
+		long firstList = currentPage * listCount + 1;
+		long lastList = firstList + listCount + -1;
 		
-		this.currentPage = pageIndex;
-		long startPage = getStartPage(currentPage);
-		long endPage = getEndPage(currentPage);
-		
-		for(long i=startPage; i<=endPage; i++){
+		for(long i=firstList; i<=lastList; i++){
 			list.add(i);
 		}
 		return list;
 	}	
+
+	public void setDataList(List<Long> dataList) {
+	}
+
+	public long getNextPage() {
+		return nextPage;
+	}
+
+	public void setNextPage(long nextPage) {
+		this.nextPage = nextPage;
+	}
+
+	public long getPrePage() {
+		return prePage;
+	}
+
+	public void setPrePage(long prePage) {
+		this.prePage = prePage;
+	}
+
 	
 }
--- src/test/java/vo/PageTest.java
+++ src/test/java/vo/PageTest.java
@@ -8,61 +8,88 @@ import org.junit.Ignore;
 import org.junit.Test;
 
 public class PageTest {
+	
+	@Test
+	public void 생성자테스트() {
+		System.out.println("/** 생성자 테스트 **/");
+		Page page1 = new Page();
+		Page page2 = new Page(1000l, 10l, 5l);
+		System.out.println("화면에 보여질 페이지 갯수: " + page1.getPageCount() + ", 한 페이지 당 리스트 갯수: " + page1.getListCount());
+		System.out.println("화면에 보여질 페이지 갯수: " + page2.getPageCount() + ", 한 페이지 당 리스트 갯수: " + page2.getListCount());
+	}
 
 	@Test 
 	/** 현재 페이지의 시작 번호를 가져오는 메소드 **/
-	public void getStartPage() {
-		System.out.println("/** 현재 페이지의 시작 번호를 가져오는 메소드 **/");
-		Page page = new Page(1852, 10, 7);
-		System.out.println("1852개의 전체 페이지 No : " + page.totalPage);
+	public void 현재페이지_시작번호() {
+		System.out.println("/** 현재 페이지 시작 번호 **/");
+		Page page = new Page(2057234666668774440l, 10l, 20l);
+		System.out.println("totalPage: " + page.getTotalPage());
+		page.setCurrentPage(100l);
 		System.out.println("현재 페이지 No : " + page.getCurrentPage());
-		long startPage = page.getStartPage(page.currentPage); 
+		long startPage = page.getStartPage(); 
 		System.out.println("현재 페이지의 시작 번호 : " + startPage);
-		assertEquals(71, startPage);
+		assertEquals(2001l, page.getStartPage());
 	}
 	
 	@Test
 	/** 현재 페이지의 마지막 번호를 가져오는 메소드 **/
-	public void getEndPage() {
-		System.out.println("/** 현재 페이지의 마지막 번호를 가져오는 메소드 **/");
-		Page page = new Page(3325, 10, 9);
-		System.out.println("3325개의 전체 페이지 No : " + page.totalPage);
-		assertEquals(333, page.totalPage);
-		long endPage = page.getEndPage(9);
-		System.out.println("현재 페이지: " + page.currentPage);
+	public void 현재페이지_마지막번호() {
+		System.out.println("/** 현재 페이지 마지막 번호 **/");
+		Page page = new Page(5000000000000000000l, 10l, 10l);
+		System.out.println("5000000000000000000l개의 전체 페이지 No : " + page.getTotalPage());
+		page.setCurrentPage(5l);
+		System.out.println("현재 페이지 No: " + page.getCurrentPage());
+		long endPage = page.getEndPage();
 		System.out.println("페이지 마지막 번호: " + endPage);
-		assertEquals(100, endPage);
+		assertEquals(60l, page.getEndPage());
 	}
 	
-	@Test
+	@Test 
 	/** 현재 페이지 목록을 가져오는 메소드 **/
-	public void getCurrentPageList() {
-		System.out.println("/** 현재 페이지 목록을 가져오는 메소드 **/");
-		Page page = new Page(20001, 10, 15);
-		List<Long> list = page.getdataList(page.currentPage);
+	public void 현재페이지_리스트() {
+		System.out.println("/** 현재 페이지 리스트 **/");
+		Page page = new Page(300l, 10l, 30l);
+		page.setCurrentPage(1l);
+		List<Long> list = page.getDataList(page.getCurrentPage());
 		System.out.println("현재 페이지 목록 리스트: " + list);
 		
 	}
 	
-	@Test
+	@Test 
 	/** 현재 페이지의 다음페이지와 이전 페이지를 가져오는 메소드 **/
-	public void getNextAndPrePage() {
-		System.out.println("/** 현재 페이지의 다음페이지와 이전 페이지를 가져오는 메소드 **/");
-		Page page = new Page(1999, 10, 5);
-		long totalPage = page.totalPage;
-		long nextPage = page.getNextPage();
+	public void 이전_목록번호() {
+		System.out.println("/** 이전 페이지의 리스트 **/");
+		Page page = new Page(40l, 10l, 10l);
+		page.setCurrentPage(4);
+		long totalPage = page.getTotalPage();
 		long prePage = page.getPrePage();
-		System.out.println("1999개의 전체 페이지 No: " + totalPage);
-		System.out.println("다음 페이지: " + nextPage);
+		System.out.println("40l개의 totalPage: " + totalPage);
 		System.out.println("이전 페이지: " + prePage);
-		assertEquals(200, totalPage);
-		assertEquals(5, page.currentPage);
-		assertEquals(6, nextPage);
-		assertEquals(4, prePage);
+		assertEquals(4, page.getCurrentPage());
+		assertEquals(3, prePage);
 		
 		//이전 페이지의 리스트
 		System.out.println("원래 페이지 번호: " + page.getCurrentPage());
-		List<Long> list = page.getdataList(prePage);
+		List<Long> list = page.getDataList(prePage);
+		System.out.println("이전 페이지 목록의 리스트: " + list);
+	}
+	
+	@Test 
+	/** 현재 페이지의 다음페이지와 이전 페이지를 가져오는 메소드 **/
+	public void 다음_목록번호() {
+		System.out.println("/** 다음 페이지의 리스트 **/");
+		Page page = new Page(1000l, 10l, 20l);
+		page.setCurrentPage(4);
+		long totalPage = page.getTotalPage();
+		long nextPage = page.getNextPage();
+		System.out.println("1000l개의 totalPage: " + totalPage);
+		System.out.println("이전 페이지: " + nextPage);
+		System.out.println("현재 페이지: " + page.getCurrentPage());
+		assertEquals(5, nextPage);
+		
+		//이전 페이지의 리스트
+		System.out.println("원래 페이지 번호: " + page.getCurrentPage());
+		List<Long> list = page.getDataList(nextPage);
 		System.out.println("이전 페이지 목록의 리스트: " + list);
 	}
 }
