3a1f48498919eea5eeb30748fad80f9cf501252e
hjchoi <hjchoi@gaia3d.com>
2018-05-15 15:50:23 +0900

    테스트 케이스

--- src/test/java/vo/Page.java
+++ src/test/java/vo/Page.java
@@ -3,57 +3,123 @@ package vo;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author HJCHOI
+ * 페이징 처리
+ */
+/**
+ * @author HJCHOI
+ *
+ */
+/**
+ * @author HJCHOI
+ *
+ */
 public class Page {
 	
-	double currentPage; //현재 페이지
-	int firstIndex;//현재 페이지 시작 인덱스
-	int lastIndex;//현재 페이지 마지막 인덱스
-	int pageUnit; //현재 페이지에 보여질 목록 수
-	double totalCount; //전체 페이지 갯수
+	/** 목록의 총 건수  **/
+	long totalCount; 
 	
-	int nextPage; //다음 페이지로
-	int beforePage; //이전 페이지로
+	/** 화면에 보여질 페이지 수  **/
+	long pageNo;
 	
-	public Page() {
-		
+	/** 전체 페이지 갯수  **/
+	long totalPage;
+	
+	/** 현재 페이지 번호  **/
+	long currentPage;
+	
+	/** 현재 페이지의 시작 번호  **/
+	long startPage;
+	
+	/** 현재 페이지의 끝 번호  **/
+	long endPage;
+	
+	/** 현재 페이지 목록 리스트  **/
+	List<Long> dataList;
+	
+	/** 다음 페이지  **/
+	long nextPage;
+	
+	/** 이전 페이지  **/
+	long prePage;
+	
+	public Page(long totalCount, long pageNo) {
+		currentPage = 1;
+		this.pageNo = 10;
+		this.totalPage = ((totalCount -1) / pageNo) + 1;
 	}
 	
-//	public double totalCount(double totalContents, int pageUnit) {
-//		double totalConut = totalContents / pageUnit;
-//		//소숫점이 있으면 무조건 올림
-//		double totalPage = Math.ceil(totalConut);
-//		return totalPage;
-//	}
-//	
-//	
-//	public int currentPage(int pageUnit, double currentContents) {
-//		//전체 페이지(totalPage)에서 현재 페이지를 구하기 
-//		//71번째 글이 몇 페이지 인지
-//		int currentPage = (int) Math.ceil(totalCount(currentContents, pageUnit));
-//
-//		return currentPage;
-//	}
-//	
-//	//시작 인덱스
-//	public int firstIndex() {
-//		//65번째 글의 시작 인덱스
-//		
-//		
-//	}
+	public Page(long totalCount, long pageNo, long currentPage) {
+		this.pageNo = 10;
+		this.currentPage = currentPage;
+		this.totalPage = ((totalCount -1) / pageNo) + 1;
+		this.startPage = currentPage * pageNo + 1 ;
+		this.nextPage = currentPage + 1;
+		this.prePage = currentPage - 1;
+	}
+
+	public void setCurrentPage(long currentPage) {
+		this.currentPage = currentPage;
+	}
 	
-	public List<Long> getCurrentPageList(long currentPage) {
-		System.out.println("현재 페이지를 구하는 메소드 입니다.");
+	public long getTotalCount() {
+		return totalCount;
+	}
+
+	public long getCurrentPage() {
+		return currentPage;
+	}
+	
+	public long getStartPage() {
+		return startPage;
+	}
+	
+	public long getNextPage(){
+		return nextPage;
+	}
+	
+	public long getPrePage(){
+		return prePage;
+	}
+
+	/**
+	 * 현재 페이지의 시작 번호를 가져오는 메소드
+	 * @param currentPage
+	 * @return
+	 */
+	public long getStartPage(long currentPage) {
+		long startPage = currentPage * pageNo + 1 ;
+		this.startPage = startPage;
+		return startPage;
+	}
+	
+	/**
+	 * 현재 페이지의 마지막 번호를 가져오는 메소드
+	 * @param currentPage
+	 * @return
+	 */
+	public long getEndPage(long currentPage) {
+		long endPage = (getStartPage(currentPage) + pageNo) - 1;
+		
+		return endPage;
+	}
+	
+	/**
+	 * 현재 페이지 목록을 가져오는 메소드
+	 * @return
+	 */
+	public List<Long> getdataList(long pageIndex) {
 		List<Long> list = new ArrayList<>();
 		
-		// long, Long
-		// int, Integer
+		this.currentPage = pageIndex;
+		long startPage = getStartPage(currentPage);
+		long endPage = getEndPage(currentPage);
 		
-		//list.add(e)
-		return null;
-	}
+		for(long i=startPage; i<=endPage; i++){
+			list.add(i);
+		}
+		return list;
+	}	
 	
-	public long getLastPage() {
-		System.out.println("마지막 페이지를 구하는 메소드 입니다.");
-		return 0l;
-	}
 }
--- src/test/java/vo/PageTest.java
+++ src/test/java/vo/PageTest.java
@@ -1,45 +1,68 @@
 package vo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class PageTest {
 
+	@Test 
+	/** 현재 페이지의 시작 번호를 가져오는 메소드 **/
+	public void getStartPage() {
+		System.out.println("/** 현재 페이지의 시작 번호를 가져오는 메소드 **/");
+		Page page = new Page(1852, 10, 7);
+		System.out.println("1852개의 전체 페이지 No : " + page.totalPage);
+		System.out.println("현재 페이지 No : " + page.getCurrentPage());
+		long startPage = page.getStartPage(page.currentPage); 
+		System.out.println("현재 페이지의 시작 번호 : " + startPage);
+		assertEquals(71, startPage);
+	}
+	
 	@Test
-	public void test() {
-//		System.out.println("페이지 테스트 입니다.");
-//		
-//		double totalCount = totalCount(303, 10);
-//		System.out.println("전체 페이지 갯수: " + totalCount);
-//		
-//		int currentPage = currentPage(10, 71);
-//		System.out.println(currentPage);
-		
+	/** 현재 페이지의 마지막 번호를 가져오는 메소드 **/
+	public void getEndPage() {
+		System.out.println("/** 현재 페이지의 마지막 번호를 가져오는 메소드 **/");
+		Page page = new Page(3325, 10, 9);
+		System.out.println("3325개의 전체 페이지 No : " + page.totalPage);
+		assertEquals(333, page.totalPage);
+		long endPage = page.getEndPage(9);
+		System.out.println("현재 페이지: " + page.currentPage);
+		System.out.println("페이지 마지막 번호: " + endPage);
+		assertEquals(100, endPage);
+	}
+	
+	@Test
+	/** 현재 페이지 목록을 가져오는 메소드 **/
+	public void getCurrentPageList() {
+		System.out.println("/** 현재 페이지 목록을 가져오는 메소드 **/");
+		Page page = new Page(20001, 10, 15);
+		List<Long> list = page.getdataList(page.currentPage);
+		System.out.println("현재 페이지 목록 리스트: " + list);
 		
 	}
 	
 	@Test
-	public void lastpageTest() {
-		System.out.println("마지막 페이지 호출 테스트 입니다.");
-		Page page = new Page();
-		page.getLastPage();
+	/** 현재 페이지의 다음페이지와 이전 페이지를 가져오는 메소드 **/
+	public void getNextAndPrePage() {
+		System.out.println("/** 현재 페이지의 다음페이지와 이전 페이지를 가져오는 메소드 **/");
+		Page page = new Page(1999, 10, 5);
+		long totalPage = page.totalPage;
+		long nextPage = page.getNextPage();
+		long prePage = page.getPrePage();
+		System.out.println("1999개의 전체 페이지 No: " + totalPage);
+		System.out.println("다음 페이지: " + nextPage);
+		System.out.println("이전 페이지: " + prePage);
+		assertEquals(200, totalPage);
+		assertEquals(5, page.currentPage);
+		assertEquals(6, nextPage);
+		assertEquals(4, prePage);
+		
+		//이전 페이지의 리스트
+		System.out.println("원래 페이지 번호: " + page.getCurrentPage());
+		List<Long> list = page.getdataList(prePage);
+		System.out.println("이전 페이지 목록의 리스트: " + list);
 	}
-	
-	
-	
-//	public double totalCount(double totalContents, int pageUnit) {
-//		double totalConut = totalContents / pageUnit;
-//		//소숫점이 있으면 무조건 올림
-//		double totalPage = Math.ceil(totalConut);
-//		
-//		return totalPage;
-//	}
-//	
-//	public int currentPage(int pageUnit, double currentContents) {
-//		//전체 페이지(totalPage)에서 현재 페이지를 구하기 
-//		//71번째 글이 몇 페이지 인지
-//		int currentPage = (int) Math.ceil(totalCount(currentContents, pageUnit));
-//
-//		return currentPage;
-//	}
-
 }
